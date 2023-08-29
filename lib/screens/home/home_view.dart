c6d7058a84ad8fd4af35d42409c9c8d38a5be63b
Juan Gonzalez Covarrubias <jgonzalezcov@gmail.com>
2023-08-28 22:34:13 -0400

    feat:a separate widget is created with the cards

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:tourist/screens/home/home_viewmodel.dart';
+import 'package:tourist/widgets/card.widget.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -21,61 +22,7 @@ class HomeView extends StatelessWidget {
                     itemCount: model.listCard.length,
                     itemBuilder: (context, index) {
                       final card = model.listCard[index];
-                      return Container(
-                          margin: const EdgeInsets.all(5),
-                          width: 150,
-                          height: 220,
-                          decoration: BoxDecoration(
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  'https://backednodeflutter-production.up.railway.app/${card.url}'),
-                              fit: BoxFit.cover,
-                            ),
-                            border: Border.all(
-                              color: Colors.green, // Color del borde
-                              width: 3.0, // Ancho del borde
-                            ),
-                          ),
-                          child: Stack(children: [
-                            Container(
-                                width: 380,
-                                height: 220,
-                                decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage(
-                                        'assets/imgs/fondo_viajes.png'),
-                                    fit: BoxFit.cover,
-                                  ),
-                                ),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      width: double.infinity,
-                                      decoration: const BoxDecoration(
-                                          color: Colors.green),
-                                      child: Text(
-                                        card.name,
-                                        style: const TextStyle(
-                                            color: Colors.white),
-                                      ),
-                                    ),
-                                    const SizedBox(
-                                      height: 10,
-                                    ),
-                                    Container(
-                                      padding: const EdgeInsets.only(left: 10),
-                                      width: 180,
-                                      child: Text(
-                                        card.descriptionShort,
-                                        style: const TextStyle(
-                                            fontFamily: 'YsabeauOffice',
-                                            color: Colors.green),
-                                      ),
-                                    )
-                                  ],
-                                ))
-                          ]));
+                      return CardWidget(cardData: card);
                     }),
               )
           ],
---
+++ lib/widgets/card.widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:tourist/model/card.Model.dart';
+
+class CardWidget extends StatefulWidget {
+  const CardWidget({Key? key, required this.cardData}) : super(key: key);
+  final MyCard cardData;
+
+  @override
+  State<CardWidget> createState() => _CardWidgetState();
+}
+
+class _CardWidgetState extends State<CardWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.all(5),
+        width: 150,
+        height: 220,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(19),
+          image: DecorationImage(
+            image: NetworkImage(
+                'https://backednodeflutter-production.up.railway.app/${widget.cardData.url}'),
+            fit: BoxFit.cover,
+          ),
+          border: Border.all(
+            color: Colors.green, // Color del borde
+            width: 3.0, // Ancho del borde
+          ),
+        ),
+        child: Stack(children: [
+          Container(
+              width: 380,
+              height: 220,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                image: const DecorationImage(
+                  image: AssetImage('assets/imgs/fondo_viajes.png'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: const BoxDecoration(
+                      color: Colors.green,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(11),
+                        topRight: Radius.circular(11),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.only(left: 15.0),
+                      child: Text(
+                        widget.cardData.name,
+                        style: const TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.only(left: 10),
+                    width: 180,
+                    child: Text(
+                      widget.cardData.descriptionShort,
+                      style: const TextStyle(
+                          fontFamily: 'YsabeauOffice', color: Colors.green),
+                    ),
+                  )
+                ],
+              ))
+        ]));
+  }
+}
